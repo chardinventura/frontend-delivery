b4d1fb07bcb002d22852678e9f5b638ba9799a44
Chardin <chardindelacruzventura829@gmail.com>
2021-04-03 22:49:45 -0400

    Added Register Activity

---
+++ app/src/main/java/com/example/frontenddelivery/activity/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.frontenddelivery.activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.frontenddelivery.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
